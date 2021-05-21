147be04b27702195244e9ae235999585d40c8a34
DodoEkambi <gkhngul2@gmail.com>
2021-05-22 01:04:17 +0300

    Değişik instance ve statik alanlara erişim örnekleri

--- CodeGym/src/Cat.java
+++ CodeGym/src/Cat.java
@@ -1,6 +1,13 @@
 
 public class Cat {
 	 private static int catCount = 0;
+	 private String fullName;
+
+	    public void setName(String firstName, String lastName) {
+	        String fullName = firstName + " " + lastName;
+	        this.fullName=fullName;
+	        //System.out.println(this.fullName);
+	    }
 
 	    public static void addNewCat() {
 	        //write your code here
@@ -8,8 +15,16 @@ public class Cat {
 	    	System.out.println(catCount);
 	        
 	    }
-
+	    public static void setCatCount(int catCount) {
+    		Cat.catCount =  catCount;
+    }
 	    public static void main(String[] args) {
-	    	
+	    	setCatCount(3);
+	    	System.out.println(Cat.catCount);
+	    	setCatCount(6);
+	    	System.out.println(Cat.catCount);
+	    	Cat cat= new Cat();
+	    	cat.setName("Dodo","Ekambi");
+	    	System.out.println(cat.fullName);
 	    }
 }
--- CodeGym/src/Cat2.java
+++
@@ -1,8 +0,0 @@
-
-public class Cat2 {
-	 private static int catCount = 0;
-
-	    public static void setCatCount(int catCount) {
-	    		Cat2.catCount=Cat2.catCount+catCount;
-	    }
-}
