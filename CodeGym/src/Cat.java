15e8960730e8383ab1eee648f4fa15ca51be40fd
DodoEkambi <gkhngul2@gmail.com>
2021-05-22 00:34:40 +0300

    First Commit
    CodeGym Sınıf Değişkeni değiştirme yöntemi.

---
+++ CodeGym/src/Cat.java
@@ -0,0 +1,15 @@
+
+public class Cat {
+	 private static int catCount = 0;
+
+	    public static void addNewCat() {
+	        //write your code here
+	    	catCount=catCount+1;
+	    	System.out.println(catCount);
+	        
+	    }
+
+	    public static void main(String[] args) {
+	    		Cat.addNewCat();
+	    }
+}
