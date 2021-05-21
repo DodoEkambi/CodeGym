7e620e36e28daba37be8f2fa15f90bb0e4838588
DodoEkambi <gkhngul2@gmail.com>
2021-05-22 00:44:00 +0300

    Cat2 static bir sınıf değişkenini object değişkeniyle arttırma
    değiştirme

--- CodeGym/src/Cat.java
+++ CodeGym/src/Cat.java
@@ -10,6 +10,6 @@ public class Cat {
 	    }
 
 	    public static void main(String[] args) {
-	    		Cat.addNewCat();
+	    	
 	    }
 }
---
+++ CodeGym/src/Cat2.java
@@ -0,0 +1,8 @@
+
+public class Cat2 {
+	 private static int catCount = 0;
+
+	    public static void setCatCount(int catCount) {
+	    		Cat2.catCount=Cat2.catCount+catCount;
+	    }
+}
